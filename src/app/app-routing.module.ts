11cec3c12096378756b6bb196cd274df19f72c22
Programmika <rothe.annika@yahoo.com>
2023-07-12 13:11:05 +0200

    added material design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
+import { PlayerComponent } from './player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartScreenComponent},
+  { path: 'game', component: GameComponent},
+  { path: 'player', component: PlayerComponent},
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
